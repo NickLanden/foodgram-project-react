2eed343137b05e0e4b0f956ba5b037d248055d2e
Никита Лопаткин <n1cklanden@yandex.ru>
2022-08-13 09:30:23 +0200

    add iingredient model tests

--- backend/foodgram/recipes/tests/test_models.py
+++ backend/foodgram/recipes/tests/test_models.py
@@ -1,9 +1,9 @@
 from django.test import TestCase
 
-from ..models import Tag
+from ..models import Ingredient, Tag
 
 
-class TagModelTest(TestCase):
+class ModelTest(TestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
@@ -14,22 +14,39 @@ class TagModelTest(TestCase):
             slug='breakfast'
         )
 
-    def test_verbose_name(self):
-        """Проверяем, что поля модели тэгов имеют читаемые названия."""
-        tag = self.breakfast_tag
-        verbose_fields = {
-            'name': 'Название тэга',
-            'color': 'Цвет',
-            'slug': 'slug'
+        cls.egg = Ingredient.objects.create(
+            name='Яйцо',
+            measurement_unit='шт.'
+        )
+
+        cls.object_list = [
+            cls.breakfast_tag,
+            cls.egg,
+        ]
+
+        cls.verbose_fields = {
+            cls.breakfast_tag: {
+                'name': 'Название тэга',
+                'color': 'Цвет',
+                'slug': 'slug'
+            },
+            cls.egg: {
+                'name': 'Название ингредиента',
+                'measurement_unit': 'Единица измерения'
+            }
         }
-        for field, expected_value in verbose_fields.items():
-            with self.subTest(field=field):
-                self.assertEqual(
-                    tag._meta.get_field(field).verbose_name,
-                    expected_value
-                )
+
+    def test_verbose_name(self):
+        """Проверяем, что поля модели имеют читаемые названия."""
+        for model_object, fields in self.verbose_fields.items():
+            for field, expected_value in fields.items():
+                with self.subTest(field=field):
+                    self.assertEqual(
+                        model_object._meta.get_field(field).verbose_name,
+                        expected_value
+                    )
 
     def test_object_name(self):
-        """Проверяем, что объект модели тэгов имеет читаемое название."""
-        tag = self.breakfast_tag
-        self.assertEqual(str(tag), tag.name)
+        """Проверяем, что объект модели имеет читаемое название."""
+        for item in self.object_list:
+            self.assertEqual(str(item), item.name)
